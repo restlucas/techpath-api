971b8510e23520b09b2926474a08d33681ad8953
Lucas Souza <restlucas.dev@gmail.com>
2025-03-12 11:15:40 -0300

    fix: star of week date

--- dist/utils/weekDate.js
+++ dist/utils/weekDate.js
@@ -1,15 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.startOfWeek = startOfWeek;
-function startOfWeek(date, weekStartsOn = 1) {
+function startOfWeek(date) {
     const result = new Date(date); // Cria uma nova data para evitar mutações
     // Converte a data local para UTC
     const utcDate = new Date(result.toISOString());
     const day = utcDate.getUTCDay(); // Retorna o dia da semana em UTC (0=Domingo, 1=Segunda, ..., 6=Sábado)
     // Calcula a diferença entre o dia atual e o dia de início da semana
-    const diff = (day < weekStartsOn ? 7 : 0) + day - weekStartsOn;
+    const diff = (day < 1 ? 7 : 0) + day - 1;
     // Ajusta a data para o início da semana em UTC
     utcDate.setUTCDate(utcDate.getUTCDate() - diff);
     utcDate.setUTCHours(0, 0, 0, 0); // Reseta para meia-noite (UTC)
-    return utcDate; // Retorna a data em UTC
+    // Ajusta a data para o fuso horário desejado
+    utcDate.setHours(utcDate.getHours() + -3); // Ajuste do fuso horário (em horas)
+    return utcDate; // Retorna a data ajustada para o fuso horário desejado
 }
--- src/utils/weekDate.ts
+++ src/utils/weekDate.ts
@@ -1,4 +1,4 @@
-export function startOfWeek(date: Date, weekStartsOn: number = 1): Date {
+export function startOfWeek(date: Date): Date {
   const result = new Date(date); // Cria uma nova data para evitar mutações
 
   // Converte a data local para UTC
@@ -7,11 +7,14 @@ export function startOfWeek(date: Date, weekStartsOn: number = 1): Date {
   const day = utcDate.getUTCDay(); // Retorna o dia da semana em UTC (0=Domingo, 1=Segunda, ..., 6=Sábado)
 
   // Calcula a diferença entre o dia atual e o dia de início da semana
-  const diff = (day < weekStartsOn ? 7 : 0) + day - weekStartsOn;
+  const diff = (day < 1 ? 7 : 0) + day - 1;
 
   // Ajusta a data para o início da semana em UTC
   utcDate.setUTCDate(utcDate.getUTCDate() - diff);
   utcDate.setUTCHours(0, 0, 0, 0); // Reseta para meia-noite (UTC)
 
-  return utcDate; // Retorna a data em UTC
+  // Ajusta a data para o fuso horário desejado
+  utcDate.setHours(utcDate.getHours() + -3); // Ajuste do fuso horário (em horas)
+
+  return utcDate; // Retorna a data ajustada para o fuso horário desejado
 }
